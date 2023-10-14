2494f38b94878aef9cbd6d786a72295113f87914
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-10-14 10:16:37 +0600

    earned about connection between server and client

---
+++ users-client/src/App.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    form.reset();
+    const user = { name, email };
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const newUsers = [...users, data];
+        setUsers(newUsers);
+      });
+  };
+
+  return (
+    <>
+      <h1>User client</h1>
+      <p>Number of users: {users.length}</p>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" /> <br />
+        <input type="email" name="email" /> <br />
+        <input type="submit" value="Add User" />
+      </form>
+      <div>
+        {users.map((user) => (
+          <p key={user.id}>
+            {user.id}: {user.name}
+          </p>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ users-server/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const users = [
+  { id: 1, name: "Sabnor", email: "sabnor@gmail.com" },
+  { id: 2, name: "Purnima", email: "purnima@gmail.com" },
+  { id: 3, name: "Mousumi", email: "mousumi@gmail.com" },
+];
+
+app.get("/", (req, res) => {
+  res.send("User server is running");
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});

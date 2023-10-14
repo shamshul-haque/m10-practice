457a61a4febe3c4cad6733d607f98b278cf695ea
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-10-14 16:26:30 +0600

    Delete A User From the Database using click button

--- crud-client/src/Users.jsx
+++ crud-client/src/Users.jsx
@@ -2,6 +2,19 @@ import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
   const users = useLoaderData();
+
+  const handleDeleteUser = (id) => {
+    console.log("Delete", id);
+    const getData = async () => {
+      const res = await fetch(`http://localhost:5000/users/${id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      console.log(data);
+    };
+    getData();
+  };
+
   return (
     <div>
       <h1>User Count: {users.length}</h1>
@@ -9,6 +22,7 @@ const Users = () => {
         {users.map((user) => (
           <p key={user._id}>
             Name: {user.name}, Email: {user.email}
+            <button onClick={() => handleDeleteUser(user._id)}>X</button>
           </p>
         ))}
       </div>
--- crud-server/index.js
+++ crud-server/index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
+// database user and password
 const uri =
   "mongodb+srv://shamshul-haque:JdNX1xGCAlysKsKm@cluster0.gwehrjf.mongodb.net/?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// create a MongoClient
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -21,19 +23,21 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // connect client to their sever
     await client.connect();
 
-    // Connect to the "insertDB" database and access its "haiku" collection
+    // get the data collection
     const database = client.db("usersDB");
     const userCollection = database.collection("users");
 
+    // read operation
     app.get("/users", async (req, res) => {
       const cursor = userCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
+    // create operation
     app.post("/users", async (req, res) => {
       const user = req.body;
       console.log("new user:", user);
@@ -41,13 +45,21 @@ async function run() {
       res.send(result);
     });
 
-    // Send a ping to confirm a successful connection
+    // delete operation
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("please delete:", id);
+      const query = { _id: ObjectId };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // send ping to confirm successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }

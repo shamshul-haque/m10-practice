0ee620faf35a46270cb9da54519c37a78d54b288
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-10-14 14:24:26 +0600

    Create client side post and Insert single userData

--- crud-client/src/App.jsx
+++ crud-client/src/App.jsx
@@ -1,9 +1,35 @@
 import React from "react";
 
 const App = () => {
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    form.reset();
+
+    const postData = async () => {
+      const res = await fetch("http://localhost:5000/users", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+      const data = await res.json();
+    };
+    postData();
+  };
+
   return (
     <>
       <h1>Hello</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" /> <br />
+        <input type="email" name="email" /> <br />
+        <input type="submit" value="Add User" />
+      </form>
     </>
   );
 };
